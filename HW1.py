4523e20ff8e6f1dc286f1ed728f6fea190a32ae0
hndkstn <92273632+hndkstn@users.noreply.github.com>
2021-10-22 22:26:54 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,75 @@
+
+import random
+
+class Stock():
+    def __init__(self, price, symbol):
+        self.price = price
+        self.symbol = symbol
+
+class MutualFund():
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+class Portfolio(object):
+    def __init__(self):
+        self.cash = 0
+        self.mutualfunds = {}
+        self.stock = {}
+        self.transaction=[]
+
+    def addCash(self, amt):
+        self.amt=amt
+        self.cash = self.cash + amt
+        self.transaction.append("$" + str(amt) + " cash is added. Balance: $" + str(round(self.cash,1)))
+
+    def withdrawCash(self, amt):
+        self.amt=amt
+        self.cash = self.cash - amt
+        self.transaction.append("$" + str(amt) + " cash is withdrawn. Balance: $" + str(round(self.cash,1)))
+
+    def buyStock(self, share, stock ):
+        self.share = share
+        self.symbol= stock.symbol
+        self.price = stock.price
+        self.cash = self.cash - self.price * self.share
+        self.transaction.append(str(share) + " shares of " + str(self.symbol) + " stock is bought. Balance: $" + str(round(self.cash,1)))
+
+    #def sellStock(self, symbol , share ):
+    #    self.share = share
+    #    self.symbol = symbol
+    #    self.first_price = self.stock.price
+    #    self.cash = self.cash + self.first_price * random.uniform(0.5, 1.5) * self.share
+    #    self.transaction.append(str(share) + " shares of " + str(self.symbol) + " stock is sold. Balance: $" + str(round(self.cash,1)))
+
+    def buyMutualFund(self,share,mutualfunds):
+        self.share = share
+        self.symbol = mutualfunds.symbol
+        self.cash = self.cash - self.share
+        self.transaction.append(str(share) + " shares of " + str(self.symbol) + " mutualfund is bought. Balance: $" + str(round(self.cash,1)))
+
+    def sellMutualFund(self,symbol,share):
+        self.share = share
+        self.symbol = symbol
+        self.cash = self.cash + self.share * random.uniform(0.9, 1.2)
+        self.transaction.append(str(share) + " shares of " + str(self.symbol) + " mutualfund is sold. Balance: $" + str(round(self.cash,1)))
+
+    def history(self):
+        print( " " + "\n ".join(self.transaction))
+
+portfolio = Portfolio()  #Creates a new portfolio
+
+portfolio.addCash(300.50)  #Adds cash to the portfolio
+
+s = Stock(20, "HFH")  #Creates Stock with price 20 and symbol "HFH"
+portfolio.buyStock(5, s)  #Buys 5 shares of stock s
+
+mf1 = MutualFund("BRT")  #Create MF with symbol "BRT"
+mf2 = MutualFund("GHT")  #Create MF with symbol "GHT"
+portfolio.buyMutualFund(10.3, mf1)  #Buys 10.3 shares of "BRT"
+portfolio.buyMutualFund(2, mf2)  #Buys 2 shares of "GHT"
+
+portfolio.sellMutualFund("BRT", 3)
+#portfolio.sellStock("HFH", 1)
+portfolio.withdrawCash(50)
+
+portfolio.history()
